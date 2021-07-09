22a386e0c1bdc534fb1a32871beb7b19839677a1
WoonHak Kong <woonhak.kong@georgebrown.ca>
2021-07-10 01:12:03 +0900

    EnemyHumanAI is modified

--- SDL_Project_Napkin/EnemyHumanAI.cpp
+++ SDL_Project_Napkin/EnemyHumanAI.cpp
@@ -18,40 +18,53 @@ void EnemyHumanAI::update()
 	glm::vec2 playerPosition = m_self->getParent()->getPlayer()->getCenterPosition();
 	glm::vec2 selfPosition = m_self->getCenterPosition();
 
-	int distance = abs(playerPosition.x - selfPosition.x);
+	int distanceX = abs(playerPosition.x - selfPosition.x);
+	int distanceY = abs(playerPosition.y - selfPosition.y);
+	float distance = sqrt(distanceX * distanceX + distanceY * distanceY);
 	int direction = 0;
 	//std::cout << "distance : " << distance << std::endl;
-	if (distance > 500)
-	{
-		m_self->idle();
-	}
-	else
-	{
-		direction = playerPosition.x - selfPosition.x;
-		if (distance > 50)
-		{
 
-			if (m_previousPostion.x == selfPosition.x && m_self->getCurrentState() == CharacterState::RUN)
-			{
-				m_self->jump();
-			}
-			if (direction < 0)
-			{
-				m_self->moveToLeft();
-			}
-			else if (direction > 0)
-			{
-				m_self->moveToRight();
-			}
-			m_previousPostion = selfPosition;
+	if (playerPosition.y > selfPosition.y - 100 && playerPosition.y < selfPosition.y + 100)
+	{
+
+
+		if (distance > 500)
+		{
+			m_self->idle();
 		}
 		else
 		{
+			direction = playerPosition.x - selfPosition.x;
+			if (distance > 50)
+			{
 
-			m_self->attack();
+				if (m_previousPostion.x == selfPosition.x && m_self->getCurrentState() == CharacterState::RUN)
+				{
+					m_self->jump();
+				}
+				if (direction < 0)
+				{
+					m_self->moveToLeft();
+				}
+				else if (direction > 0)
+				{
+					m_self->moveToRight();
+				}
+				m_previousPostion = selfPosition;
+			}
+			else
+			{
+
+				m_self->attack();
+
+			}
 
 		}
-
 	}
+	else
+	{
+		m_self->idle();
+	}
+
 
 }
